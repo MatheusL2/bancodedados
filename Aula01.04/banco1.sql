15b1f5dbc5414832d89c2c73c8cec6eecb39daf1
MatheusL2 <45121206+MatheusL2@users.noreply.github.com>
2026-04-01 20:58:41 -0300

    Create banco1.sql

---
+++ Aula01.04/banco1.sql
@@ -0,0 +1,20 @@
+/*INSERE DADOS NO BD */
+INSERT INTO usuario(nome, email, senha)
+VALUES 
+("João", "joao@rmail.com", "123"),
+("Pedro", "pedro@rmail.com", "123");
+
+INSERT INTO autor(nome)
+VALUES ("Santos Agostinho"),
+	   ("Aristoteles");
+       
+INSERT INTO autor(nome)
+VALUES ("Santos Agostinho"),
+	   ("Aristoteles");
+INSERT INTO editora(nome)
+VALUES ("L&M"),
+	   ("Companhia de Letras");
+INSERT INTO categoria(nome)
+VALUES ("Filosofia"),
+	   ("Religião");
+       

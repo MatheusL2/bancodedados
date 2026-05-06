158786b5dbd946937da1a2ceff9b2dd94ad8c4f6
MatheusL2 <45121206+MatheusL2@users.noreply.github.com>
2026-05-06 20:55:15 -0300

    Update script_select.sql

--- Atividade 2204/script_select.sql
+++ Atividade 2204/script_select.sql
@@ -1,15 +1,45 @@
-SELECT
-    t2.nome AS nome_autor,
-    t1.titulo,
-    t3.nome AS nome_categoria,
-    t4.nome AS nome_usuario,
-    t5.nome AS nome_editora
-FROM livro t1
-JOIN autor t2 
-    ON t1.id_autor = t2.id_autor
-JOIN categoria t3 
-    ON t1.id_categoria = t3.id_categoria
-JOIN usuario t4 
-    ON t1.id_usuario = t4.id_usuario
-JOIN editora t5 
-    ON t1.id_editora = t5.id_editora;
+USE biblioteca_pessoal_1s2026;
+
+SELECT * 
+FROM livro;
+
+SELECT 
+    T1.ID_USUARIO,
+    T2.NOME,
+    COUNT(T1.ID_LIVRO) AS QTD_LIVRO,
+    SUM(T1.LIDO) AS QTD_LIVRO_LIDO,
+
+    ROUND(
+        (SUM(T1.LIDO) / COUNT(T1.ID_LIVRO)) * 100,
+        2
+    ) AS PCT_LIDO,
+
+    MAX(T1.ANO_PUBLICACAO) AS ANO_MAIS_RECENTE
+
+FROM LIVRO T1
+
+JOIN USUARIO T2 
+    ON T1.ID_USUARIO = T2.ID_USUARIO
+
+GROUP BY 
+    T1.ID_USUARIO,
+    T2.NOME;
+
+/*
+CALCULA A IDADE DOS AUTORES
+*/
+
+SELECT 
+    NOME,
+    ANO_NASCIMENTO,
+    ANO_MORTE,
+
+    (
+        IF(
+            ANO_MORTE > 0,
+            ANO_MORTE,
+            YEAR(CURRENT_DATE())
+        ) - ANO_NASCIMENTO
+    ) AS IDADE
+
+FROM AUTOR;
